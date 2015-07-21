50c35bd18380dc67832b935917972d6385a85ee6
Grayson Earle <graysonearle@gmail.com>
2015-07-21 15:13:53 -0400

    more updates!

--- Protest Protest Revolution/Assets/random_gen.js
+++ Protest Protest Revolution/Assets/random_gen.js
@@ -1,14 +1,12 @@
-﻿// Starting in 2 seconds.
-// a projectile will be launched every 0.3 seconds
-
-var projectile : Transform;
-
-InvokeRepeating("LaunchProjectile", 2, 2);
+﻿var projectile : Transform;
+// invoke rep (take # seconds to start, then every # seconds interval
+InvokeRepeating("LaunchProjectile", 0, 2);
 
 function LaunchProjectile () {
 	var randomX : float = Random.Range(-10,10);
-	var randomZ : float = Random.Range(-10,10);
+	var randomZ : float = 2;
 	
 	var instance : Transform = Instantiate(projectile, new Vector3(randomX, 15, randomZ), Quaternion.identity);
+	instance.transform.rotation = Random.rotation;
 	//instance.velocity = Random.insideUnitSphere * 5;
 }
