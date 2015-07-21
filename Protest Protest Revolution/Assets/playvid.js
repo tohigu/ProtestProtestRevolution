84dbfeb5abd55a851311fa7cecab226a1a54e9ee
Grayson Earle <graysonearle@gmail.com>
2015-07-21 15:23:07 -0400

    fixed unity to work with git (maybe?)

--- Protest Protest Revolution/Assets/playvid.js
+++
@@ -1,9 +0,0 @@
-﻿// Assigns a movie texture to the current material and plays it.
-
-var movTexture : MovieTexture;
-
-function Start () {
-	GetComponent.<Renderer>().material.mainTexture = movTexture;
-	movTexture.Play();
-	movTexture.loop = true;
-}
---
+++ Protest Protest Revolution/Assets/scripts/playvid.js
@@ -0,0 +1,9 @@
+﻿// Assigns a movie texture to the current material and plays it.
+
+var movTexture : MovieTexture;
+
+function Start () {
+	GetComponent.<Renderer>().material.mainTexture = movTexture;
+	movTexture.Play();
+	movTexture.loop = true;
+}
