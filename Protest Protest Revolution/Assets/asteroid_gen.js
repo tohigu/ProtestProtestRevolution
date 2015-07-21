611fe1dfac24efa03085761e5420cf1a9ecfb5c5
Grayson Earle <graysonearle@gmail.com>
2015-07-21 14:56:11 -0400

    added ignore file

---
+++ Protest Protest Revolution/Assets/asteroid_gen.js
@@ -0,0 +1,14 @@
+﻿// Starting in 2 seconds.
+// a projectile will be launched every 0.3 seconds
+
+var projectile : Transform;
+
+InvokeRepeating("LaunchProjectile", 2, 2);
+
+function LaunchProjectile () {
+	var randomX : float = Random.Range(-10,10);
+	var randomZ : float = Random.Range(-10,10);
+	
+	var instance : Transform = Instantiate(projectile, new Vector3(randomX, 15, randomZ), Quaternion.identity);
+	//instance.velocity = Random.insideUnitSphere * 5;
+}
