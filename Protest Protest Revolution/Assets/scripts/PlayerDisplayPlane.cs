436a5e79e54d84a44c1df7b35a2e13bed5ec1cfd
tohigu <hello@tonyhiguchi.com>
2015-07-23 14:20:38 -0400

    Added kinect

---
+++ Protest Protest Revolution/Assets/scripts/PlayerDisplayPlane.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDisplayPlane : MonoBehaviour {
+
+    public KinectManager kinect;
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.material.mainTexture = kinect.GetUsersLblTex();
+	}
+}
